f2337f46b5d22afbe8c1944a8e078cb22086627c
kevin-martins <kev.martinsvieira@gmail.com>
2022-11-26 17:11:18 +0100

    feat: fetch word and display it on screen

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,14 +2,24 @@ import { useEffect, useState } from "react"
 import DisplayWord from "./DisplayWord"
 
 const Game = () => {
-    const [word, setWord] = useState<string>('  hello  ooo')
-    const [userInput, setUserInput] = useState<string>('')
+    const [word, setWord] = useState<string>('')
+    // const [userInput, setUserInput] = useState<string>('')
 
-    // const currentWord = await fetch('https://random-word-api.herokuapp.com/word')
-    // setWord(currentWord)
+    useEffect(() => {
+        const getWord = async (): Promise<void> => {
+            await fetch('https://random-word-api.herokuapp.com/word')
+                .then(res => res.json())
+                .then(res => res.toString())
+                .then(res => setWord(res))
+        }
+        getWord()
+    }, [])
+
+    console.log(word)
+
     return (
         <>
-            <input type="text" value="" />
+            {/* <input type="text" value="" /> */}
             <DisplayWord word={word.split('')} />
         </>
     )
