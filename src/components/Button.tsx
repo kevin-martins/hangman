483de685f6b64d07bdd7354fdfadad124dbed0e0
kevin-martins <kev.martinsvieira@gmail.com>
2022-11-28 23:21:39 +0100

    feat: takes an array of actions as props and highlight itself if already selected

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,35 +1,39 @@
-import { useSelector } from "react-redux"
-import { DifficultyProps } from "../features/hangmanSlice"
+import { useAppDispatch, useAppSelector } from "../app/hooks"
+import { Difficulty } from "../models/difficulties"
 import '../styles/button.css'
 
-
 type Props = {
-	text: DifficultyProps
+	element: Difficulty | string
+	actions: any[]
+	selected?: boolean
 }
 
-const Button = ({ text }: Props) => {
-	const setColor = (difficulty: number) => {
+const Button = ({ element, actions, selected = false }: Props) => {
+	const difficulty = useAppSelector(state => state.hangman.difficulty)
+	const dispatch = useAppDispatch();
+	const setColor = (difficulty: number | string): string => {
 		switch (difficulty) {
-			case DifficultyProps.EASY:
-				return 'text-green-500'
-			case DifficultyProps.CHALLENGING:
-				return 'text-yellow-500'
-			case DifficultyProps.HARD:
-				return 'text-red-500'
-			case DifficultyProps.EXTREME:
-				return 'text-gray-800'
-			default:
-				break
+			case Difficulty.EASY: return 'text-lime-500'
+			case Difficulty.CHALLENGING: return 'text-yellow-500'
+			case Difficulty.HARD: return 'text-red-500'
+			default: return 'text-white hover:text-black'
 		}
 	}
+
+	const handleClick = () => {
+		actions.forEach(action => {
+			dispatch(action)
+		})
+	}
+
     return (
-        <button className="mx-auto bg-gray-600">
+        <button className="mx-auto bg-gray-600" onClick={handleClick}>
 			<span className="">
 				<span
 					data-attr-color='white'
-					className={`${setColor(text)} font-semibold z-10`}
+					className={`${setColor(element)} ${selected && 'selected'} font-semibold z-10`}
 				>
-					{DifficultyProps[text]}
+					{typeof element === "number" ? Difficulty[element] : element.toUpperCase()}
 				</span>
 			</span>
 		</button>
