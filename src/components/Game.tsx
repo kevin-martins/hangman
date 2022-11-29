d70aab5c96713801ae3060e6b28443a76efd9207
kevin-martins <kev.martinsvieira@gmail.com>
2022-11-29 18:07:17 +0100

    feat: fetch word based on difficulty and init wordProgression state

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import { setGameState } from '../features/hangman-slice'
-import { GameState } from '../models/game-state'
+import { setGameState, setWordProgression } from '../features/hangman-slice'
+import { useFetchWordsQuery } from '../features/words/word-api-slice'
 import Button from './Button'
+import Loading from './Loading'
+import DisplayWord from './DisplayWord'
+import { DifficultyProps } from '../models/difficulty'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { GameState } from '../models/game-state'
+
+
+const wordDifficulty = (difficulty: DifficultyProps): number => {
+  if (difficulty === DifficultyProps.EASY) return 6
+  else if (difficulty === DifficultyProps.CHALLENGING) return 9
+  return 10
+}
 
 const Game = (): JSX.Element => {
+  const difficulty = useAppSelector(state => state.hangman.difficulty)
+  const { data = [], isFetching } = useFetchWordsQuery(wordDifficulty(difficulty))
+
+  const dispatch = useAppDispatch();
+  if (!isFetching) {
+    const progression = data[0].split('').map(() => { return { letter: '_', color: 'text-white' } })
+    dispatch(setWordProgression(progression))
+  }
+
   return (
-    <div>
+    <div className='h-full'>
+      <DisplayWord />
       <Button element="back" actions={[setGameState(GameState.MENU)]} />
     </div>
   )
