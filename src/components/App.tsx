7a1b055feca8ee319bb4772f50da8fdf055f0a52
kevin-martins <kev.martinsvieira@gmail.com>
2022-11-29 18:08:58 +0100

    feat: move Play page out of Center for better control

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,9 @@
-import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { useAppSelector } from '../app/hooks'
 import { GameState } from '../models/game-state';
 import Center from './Center';
 import Menu from './Menu';
-import GameDifficulty from './GameDifficulty';
-import GameDashboard from './GameDashboard';
+import Difficulty from './Difficulty';
+import Dashboard from './Dashboard';
 import Game from './Game'
 import Settings from './Settings';
 
@@ -11,14 +11,16 @@ const App = (): JSX.Element => {
   const gameState = useAppSelector(state => state.hangman.gameState)
 
   return (
-    <body>
-      <Center>
-        {gameState === GameState.MENU && <Menu />}
-        {gameState === GameState.PLAY && <Game />}
-        {gameState === GameState.DIFFICULTY_SELECTION && <GameDifficulty />}
-        {gameState === GameState.DASHBOARD && <GameDashboard />}
-        {gameState === GameState.SETTINGS && <Settings />}
-      </Center>
+    <body className='bg-gray-700 h-screen'>
+      {gameState === GameState.PLAY ?
+        <Game /> :
+        <Center>
+          {gameState === GameState.MENU && <Menu />}
+          {gameState === GameState.DIFFICULTY_SELECTION && <Difficulty />}
+          {gameState === GameState.DASHBOARD && <Dashboard />}
+          {gameState === GameState.SETTINGS && <Settings />}
+        </Center>
+      }
     </body>
   )
 }
