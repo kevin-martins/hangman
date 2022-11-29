d88fd3f8c89c1379ec30673fe833fb24775ffd15
kevin-martins <kev.martinsvieira@gmail.com>
2022-11-29 18:05:01 +0100

    feat: change center style for better control over its childrens

--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -1,7 +1,7 @@
 const Center = ({ children }: any): JSX.Element => {
   return (
-    <div className="grid bg-gray-700 h-screen mx-auto place-items-center">
-        <div className="flex flex-col gap-4 sm:w-2/3 max-w-screen-xl mx-auto">
+    <div className="grid h-full place-items-center">
+        <div className="flex flex-col gap-4 sm:w-2/3 max-w-screen-xl">
             {children}
         </div>
     </div>
