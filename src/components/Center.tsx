62bcbe73e93a3d88270bd5b82c44ec5567e3fe6b
kevin-martins <kev.martinsvieira@gmail.com>
2022-11-29 14:58:37 +0100

    feat: make center component responsive

--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -1,7 +1,7 @@
 const Center = ({ children }: any): JSX.Element => {
   return (
-    <div className="grid bg-gray-700 h-screen m-auto place-items-center">
-        <div className="flex flex-col gap-4">
+    <div className="grid bg-gray-700 h-screen mx-auto place-items-center">
+        <div className="flex flex-col gap-4 sm:w-2/3 max-w-screen-xl mx-auto">
             {children}
         </div>
     </div>
