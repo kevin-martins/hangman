da563c6132ff8e35ace7bef0e802c0ce8efcde87
kevin-martins <kev.martinsvieira@gmail.com>
2022-11-30 17:32:17 +0100

    refactor: spaces

--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -1,9 +1,9 @@
 const Center = ({ children }: any): JSX.Element => {
   return (
     <div className="grid h-full place-items-center">
-        <div className="flex flex-col gap-4 sm:w-2/3 max-w-screen-xl">
-            {children}
-        </div>
+      <div className="flex flex-col gap-4 sm:w-2/3 max-w-screen-xl">
+        {children}
+      </div>
     </div>
   )
 }
