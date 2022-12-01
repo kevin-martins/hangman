1518df2cc3813f23a73f562cb3853233d777e575
kevin-martins <kev.martinsvieira@gmail.com>
2022-12-01 10:45:54 +0100

    feat: adding own route for game

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { store } from './app/store';
 import App from './components/App';
+import Game from './components/Game';
 import './styles/index.css';
 
 const container = document.getElementById('root')!;
@@ -11,7 +13,15 @@ const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-        <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<App />} />
+            <Route path="game" element={<Game />} />
+            {/* <Route path="*" element={<NoPage />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </React.StrictMode>
   </Provider>
 );
