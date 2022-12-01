05bb1445d18f52cdac7c34b0eeff1a2b33972a29
kevin-martins <kev.martinsvieira@gmail.com>
2022-12-01 15:41:17 +0100

    refactor: move binary condition in Game

--- src/components/Restart.tsx
+++ src/components/Restart.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from "../app/hooks"
-import { setGameState, setPlayerTurn, setReset } from "../features/hangman-slice"
+import { setGameState, setPlayerTurn, setReset, setWinnerState } from "../features/hangman-slice"
 import { capitalize } from "../helpers/helpers"
 import { GameState } from "../models/game-state"
 import { WinningState } from "../models/winner-state"
@@ -9,6 +9,7 @@ import Button from "./Button"
 
 export const Restart = (): JSX.Element => {
   const winner = useAppSelector(state => state.hangman.winner)
+  const word = useAppSelector(state => state.hangman.word)
   const dispatch = useAppDispatch()
 
   useEffect(() => {
@@ -17,18 +18,15 @@ export const Restart = (): JSX.Element => {
   }, [winner])
 
   return (
-    <>
-      {winner !== WinningState.NONE && <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-black/30 sm:w-96 w-72 h-60 z-50`}>
-        <div className='flex flex-col gap-2 text-white mx-auto'>
-          <h2 className='text-center text-2xl border-b-[.5px] border-white py-2 mx-10'>{capitalize(WinningState[winner])} won</h2>
-          <Link to="/" className="mx-auto">
-            <Button element="Dashboard" actions={[setReset(), setGameState(GameState.DASHBOARD)]} />
-          </Link>
-          <Button element="Menu" actions={[setReset(), setGameState(GameState.MENU)]} />
-          <Button element="Restart" actions={[setReset(), setGameState(GameState.RESTART)]} />
-        </div>
-      </div>}
-    </>
+    <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-black/30 sm:w-96 w-72 pb-3 z-50`}>
+      <div className='flex flex-col gap-2 text-white mx-auto'>
+        <h2 className='text-center text-2xl border-b-[.5px] border-white py-2 mx-10'>{capitalize(WinningState[winner])} won</h2>
+        {winner === WinningState.COMPUTER && <p className='text-center'>the word was: {word}</p>}
+        <Button element="Dashboard" actions={[setReset(), setGameState(GameState.DASHBOARD)]} />
+        <Button element="Menu" actions={[setReset(), setGameState(GameState.MENU)]} />
+        <Button element="Restart" actions={[setReset(), setGameState(GameState.RESTART)]} />
+      </div>
+    </div>
   )
 }
 
