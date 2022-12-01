6e43aa4260ff7ba4cd9ab616b2f8bee41ba960d8
kevin-martins <kev.martinsvieira@gmail.com>
2022-12-01 18:05:44 +0100

    fix: removing useless imports

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,18 +1,19 @@
-import { useAppDispatch, useAppSelector } from "../app/hooks"
+import { useAppSelector } from "../app/hooks"
 import { setGameState } from "../features/hangman-slice"
 import { GameState } from "../models/game-state"
 import Button from "./Button"
 
-const Menu = () => {
-    const difficulty = useAppSelector(state => state.hangman.difficulty)
-    return (
-        <>
-            <Button element="Play" actions={[setGameState(GameState.PLAY)]} />
-            <Button element={difficulty} actions={[setGameState(GameState.DIFFICULTY_SELECTION)]} />
-            <Button element="Dashboard" actions={[setGameState(GameState.DASHBOARD)]} />
-            <Button element="Settings" actions={[setGameState(GameState.SETTINGS)]} />
-        </>
-    )
+const Menu = (): JSX.Element => {
+  const difficulty = useAppSelector(state => state.hangman.difficulty)
+  return (
+    <>
+      <h1 className="text-center text-6xl">Hangman</h1>
+      <Button element="Play" actions={[setGameState(GameState.PLAY)]} />
+      <Button element={difficulty} actions={[setGameState(GameState.DIFFICULTY_SELECTION)]} />
+      <Button element="Dashboard" actions={[setGameState(GameState.DASHBOARD)]} />
+      <Button element="Settings" actions={[setGameState(GameState.SETTINGS)]} />
+    </>
+  )
 }
 
 export default Menu
