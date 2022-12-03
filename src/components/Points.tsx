c7dc869f9b7276501fd2fe576be0fc74da85a32c
kevin-martins <kev.martinsvieira@gmail.com>
2022-12-03 01:45:57 +0100

    feat: point animation on win

--- src/components/Points.tsx
+++ src/components/Points.tsx
@@ -1,13 +1,31 @@
-import { useAppSelector } from '../app/hooks'
+import { useEffect, useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { setPoints } from '../features/hangman-slice'
+import { WinningState } from '../models/winner-state'
+import '../styles/points.css'
 
 const Points = (): JSX.Element => {
     const points = useAppSelector(state => state.hangman.points)
+    const winner = useAppSelector(state => state.hangman.winner)
+    const [animation, setAnimation] = useState(false)
+    const dispatch = useAppDispatch()
+    
+    useEffect(() => {
+        if (winner === WinningState.PLAYER)
+            dispatch(setPoints())
+    }, [winner])
+
+    useEffect(() => {
+        setAnimation(true)
+        setTimeout(() => {
+            setAnimation(false)
+        }, 1200)
+    }, [points])
 
     return (
         <div className='flex flex-row w-full py-8 px-12'>
-            <span className='h-full my-auto ml-auto text-white pr-1'>{points}</span>
+            <p className={`${animation && 'growth'} h-full my-auto ml-auto text-yellow-500 text-xl pr-1`}>{points}</p>
             <img
-                className=''
                 src="https://img.icons8.com/emoji/48/null/coin-emoji.png"
                 alt="gold image"
                 height={32}
